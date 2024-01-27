7d2582490529319007358f6eb20ea8347118bf84
Harikrishnan <harivk1998@gmail.com>
2024-01-27 11:46:46 +0530

    trafficLight

---
+++ src/TrafficLight.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+
+function Light({ backgroundColor }) {
+  return (
+    <div
+      aria-hidden={true}
+      className="traffic-light"
+      style={{ backgroundColor }}
+    />
+  );
+}
+
+const TrafficLight = ({
+  initialColor = "green",
+  config,
+  layout = "vertical",
+}) => {
+  const [currentColor, setCurrentColor] = useState(initialColor);
+
+  useEffect(() => {
+    const { duration, next } = config[currentColor];
+    const timerId = setTimeout(() => {
+      setCurrentColor(next);
+    }, duration);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [currentColor]);
+
+  return (
+    <div
+      aria-live="polite"
+      aria-label={`Current light:${currentColor}`}
+      className={[
+        `traffic-light-container`,
+        layout === "vertical" && "traffic-light-container--vertical",
+      ]
+        .filter((cls) => !!cls)
+        .join(" ")}
+    >
+      {Object.keys(config).map((color) => (
+        <Light
+          key={color}
+          backgroundColor={
+            color === currentColor ? config[color].backgroundColor : undefined
+          }
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TrafficLight;
+
